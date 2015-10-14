3080e8b73b5ac27575fb41a2d56dd8ae0d14283a
zmpeg <zandstra.matt@gmail.com>
2015-10-14 11:20:43 -0600

    the server now starts and does nothing

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const routes = require('./routes');
+const path = require('path');
+const http = require('http');
+
+const app = module.exports = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const env = process.env.NODE_ENV || 'development';
+
+app.get('*', routes.index);
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
